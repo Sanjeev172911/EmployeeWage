554bfb9d301845bbc5820d9e60e78a23a679b5c7
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-20 14:38:38 +0530

    completed use case 2

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,8 +1,20 @@
 public class Employee {
+    int wagePerHour;
+    int fullDayHour;
+
+    Employee(int wagePerHour,int fullDayHour){
+        this.wagePerHour=wagePerHour;
+        this.fullDayHour=fullDayHour;
+    }
+
     public boolean isEmployeePresent(){
         double chancePresent=Math.random();
 
         if(chancePresent<.5) return false;
         return true;
+    }
+
+    public int calculateDailyWage(){
+        return wagePerHour*fullDayHour;
     }
 }
